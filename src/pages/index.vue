2c8bb640b231ed4f9e90746c147651d10495b717
ttt1091 <tez1091@yahoo.co.jp>
2021-05-20 21:02:51 +0900

    change

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,14 @@
-<template lang="pug">
-  div
-    ul(v-for="item in items" :key="item.title" target="_blank" rel="noopener noreferrer" router exact)
-      li
-        a(:href="item.title") {{ item.title }}
-        div {{ item.subtitle }}
-    div https://content.nuxtjs.org/ja
-    div pug!!
+<template>
+  <div>
+    <ul>v-for="item in items" :key="item.title" target="_blank" rel="noopener noreferrer" router exact)
+      <li>
+        <a :href="item.title">{{ item.title }}</a>
+        <div>{{ item.subtitle }}</div>
+      </li>
+    </ul>
+    <div>https://content.nuxtjs.org/ja</div>
+    <div>pug!!</div>
+  </div>
 </template>
 
 <script lang="ts">
